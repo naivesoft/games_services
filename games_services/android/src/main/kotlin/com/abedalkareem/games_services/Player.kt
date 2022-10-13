db7ededd955430ddd06d37107df730e917c49d67
naivesoft <naivesoft@163.com>
2022-10-13 22:38:05 +0800

    dev: a

--- games_services/android/src/main/kotlin/com/abedalkareem/games_services/Player.kt
+++ games_services/android/src/main/kotlin/com/abedalkareem/games_services/Player.kt
@@ -1,11 +1,17 @@
 package com.abedalkareem.games_services
 
 import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import com.abedalkareem.games_services.util.PluginError
 import com.abedalkareem.games_services.util.errorCode
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.images.ImageManager
 import com.google.android.gms.games.Games
 import io.flutter.plugin.common.MethodChannel
+import java.io.ByteArrayOutputStream
+import java.util.HashMap
 
 class Player {
 
@@ -38,9 +44,27 @@ class Player {
     Games.getPlayersClient(activity, lastSignedInAccount)
       .currentPlayer
       .addOnSuccessListener { player ->
-        result.success(player.hiResImageUri)
+        readImage(player.hiResImageUri, activity, result)
+//        result.success(player.hiResImageUri)
       }.addOnFailureListener {
         result.error(PluginError.FailedToGetPlayerAvatar.errorCode(), it.localizedMessage, null)
       }
+  }
+
+  fun readImage(uri: Uri?, activity: Activity?, result: MethodChannel.Result) {
+    val data: MutableMap<String, Any?> = HashMap()
+    if (uri == null) {
+      data["bytes"] = null
+      result(data)
+    }
+    val manager = ImageManager.create(activity!!)
+    manager.loadImage({ uri1, drawable, isRequestedDrawable ->
+      val bitmap = (drawable as BitmapDrawable?)!!.bitmap
+      val stream = ByteArrayOutputStream()
+      bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+      val bytes = stream.toByteArray()
+      data["bytes"] = bytes
+      result(data)
+    }, uri!!)
   }
 }
